abeeab40cab667cee7e1c0bc317ae8426257bb98
jim90247 <jim90247@gmail.com>
2021-07-05 21:09:56 +0800

    Update host addresses in hello_world

--- hello_world/common.h
+++ hello_world/common.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "rpc.h"
 
-static const std::string kServerHostname = "130.127.133.179";
-static const std::string kClientHostname = "130.127.133.145";
+static const std::string kServerHostname = "192.168.223.1";
+static const std::string kClientHostname = "192.168.223.2";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;
